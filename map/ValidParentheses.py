b09fa8e232db832b42859c9b0b5ee7d0c805a61b
Juli <julianalicode@gmail.com>
2024-03-22 00:05:59 -0400

    Create ValidParentheses.py

---
+++ map/ValidParentheses.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isValid(self,s:str)->bool:
+        dict = {'(':')','{':'}','[':']'}
+        stack = []
+
+        for char in s:
+            if char in dict:
+                stack.append(char)
+            elif char in dict.values():
+                if not stack or dict[stack.pop()]!=char:
+                    return False
+        return not stack
